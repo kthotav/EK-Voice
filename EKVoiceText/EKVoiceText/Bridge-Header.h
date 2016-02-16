64c842f8ae869fce8a2cc0000ef5ba25849621e2
kthotav <kthotav@gmail.com>
2016-02-16 15:17:12 -0800

    added iOS EKVoice

---
+++ EKVoiceText/EKVoiceText/Bridge-Header.h
@@ -0,0 +1,22 @@
+//
+//  Bridge-Header.h
+//  EKVoiceText
+//
+//  Created by Karthik on 2/16/16.
+//  Copyright © 2016 KarthikThota. All rights reserved.
+//
+
+//#ifndef Bridge_Header_h
+//#define Bridge_Header_h
+//
+//
+//#endif /* Bridge_Header_h */
+
+
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEAcousticModel.h>
+#import <OpenEars/OEPocketsphinxController.h>
+#import <OpenEars/OEEventsObserver.h>
+#import <OpenEars/OELogging.h>
+#import <OpenEars/OEFliteController.h>
+#import <Slt/Slt.h>
